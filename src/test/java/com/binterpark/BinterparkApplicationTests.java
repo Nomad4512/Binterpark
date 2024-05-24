4ff91c096ecc304d5cd2d86e18e7dcb71b5aa66f
Hyunbin Ban <103755593+Nomad4512@users.noreply.github.com>
2024-05-24 15:25:39 +0900

    Dockerfile, docker-compose.yml 작성, application.yml 수정, Docker 이미지 생성 후 테스트 완료

--- src/test/java/com/binterpark/BinterparkApplicationTests.java
+++ src/test/java/com/binterpark/BinterparkApplicationTests.java
@@ -2,9 +2,11 @@ package com.binterpark;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
 class BinterparkApplicationTests {
+
 
 	@Test
 	void contextLoads() {
